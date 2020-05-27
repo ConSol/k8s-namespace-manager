c00b22cdad166096681dd35fa3d68039661fe64e
Alexander Ryndin <alexander.ryndin@consol.de>
2020-05-27 17:00:26 +0200

    add properties

--- src/main/java/de/consol/labs/k8s/nsman/task/WebhookPayload.java
+++ src/main/java/de/consol/labs/k8s/nsman/task/WebhookPayload.java
@@ -1,12 +1,18 @@
 package de.consol.labs.k8s.nsman.task;
 
-import lombok.Setter;
+import de.consol.labs.k8s.nsman.crd.Policy;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
-@Setter
 @Getter
 @ToString
+@RequiredArgsConstructor
 public class WebhookPayload {
-
+  @lombok.NonNull
+  private final NamespaceManagerIdentifier namespaceManager;
+  @lombok.NonNull
+  private final Policy policy;
+  @lombok.NonNull
+  private final String namespace;
 }
